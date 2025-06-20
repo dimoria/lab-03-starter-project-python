a68507d04d79da055988a8c2d78d6169120d89c2
dimoria <143126354+dimoria@users.noreply.github.com>
2025-06-20 13:38:18 +0000

    сreated dockerfile and added chages required in labotary work

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
-
-@router.get('')
+@router.get("")
 def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+    return {"msg": "Hello, World!"}
+
+@router.get("/matrix")
+def matrix_mult() -> dict:
+    a = np.random.randint(0, 10, (10, 10)).tolist()
+    b = np.random.randint(0, 10, (10, 10)).tolist()
+    product = (np.dot(a, b)).tolist()
+    return {
+        "matrix_a": a,
+        "matrix_b": b,
+        "product": product
+    }
